c6711ece9af05bbabec1c963ca54b87704038388
unknown <sethsopheara@gmail.com>
2023-07-26 15:20:23 +0700

    add

---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const userController = require('../controllers/userControllers');
+
+const router = express();
+
+router.get('/', userController.getAll);
+
+module.exports = router;
